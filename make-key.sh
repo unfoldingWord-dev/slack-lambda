116dbdf7142c4033674e6df6de7277e67cd98cfe
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2016-08-24 10:05:54 -0400

    Deploying with travis-ci
    Install AWS CLI
    Install Apex CLI
    Add AWS environment variables to .travis.yml
    Add deploy instructions to .travis.yml
    Add deploy script

---
+++ make-key.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+########################################################################
+#
+# NAME make-key.sh  -  make travis keys from aws credentials
+#
+# USAGE: ./make-key.sh
+#
+# AWS credentials expected to be in ~/.aws/credentials
+#
+########################################################################
+
+yaml=".travis.yml"
+cred=~/.aws/credentials
+
+cp ${yaml} ${yaml}.bak
+grep -v 'secure:' ${yaml} > ${yaml}.tmp
+mv ${yaml}.tmp ${yaml}
+str=""
+
+while read line ; do
+    echo ${line}
+    key="${line%=*}"
+    val="${line#*=}"
+
+    case ${key} in
+        '['*|'#'*) ;; # ignore commented out credentials
+        *)
+            if [ "${str}" = "" ]
+            then
+                str="${key^^}=${val}"
+            else
+                str="${str} ${key^^}=${val}"
+            fi
+        ;;
+    esac
+done < ${cred}
+
+travis encrypt ${str} --add
+
+echo "Finished"
