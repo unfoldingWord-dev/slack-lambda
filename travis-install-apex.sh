645f32af0b6c973471460530815c597aa1e16c87
Phil Hopper <phillip_hopper@wycliffeassociates.org>
2016-08-24 10:51:43 -0400

    Debugging

--- travis-install-apex.sh
+++ travis-install-apex.sh
@@ -2,7 +2,7 @@
 
 LATEST=$(curl -s https://api.github.com/repos/apex/apex/tags | grep -Eo '"name":.*?[^\\]",'  | head -n 1 | sed 's/[," ]//g' | cut -d ':' -f 2)
 URL="https://github.com/apex/apex/releases/download/$LATEST/apex_linux_amd64"
-DEST=/usr/local/bin/apex
+DEST=~/.local/bin/apex
 
 curl -sL ${URL} -o ${DEST}
 chmod +x ${DEST}
